d2703bc91cea2da4093a3cace82bb2cbe46c96a6
Robel Kebede <robel.kebede@bison.howard.edu>
2023-08-08 22:18:43 +0300

    Added functional features

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from .forms import TextInputForm
 from .models import TextInput
+from .functions import summary
 
 def index(request):
     return render(request, 'index.html')
@@ -20,9 +21,26 @@ def input_form(request):
 def analysis(request, instance_id):
     instance = TextInput.objects.get(id=instance_id)
     
-    # Perform your analysis on instance.url here
-    # This is where our function would take the link (instance.url) and render our desired outputs
-    summary = instance.url
+    # Perform our features on instance.url here
+    
+
+    # First we need to call the function that will scrape the link
+    # text = scrape_function(instance.url)
+    
+    # Second we need to call the summary function to summarize the text
+    # summary = summary_function(text)
+    
+    # Third we need to call the primary and secondary viewpoints functions
+    # primary = primary_function(text)
+    # secondary = secondary_function(text)
+    
+    # Lastly, the resources function which will render the additional sources for the new viewpoints
+    # resources = resources(viewpoints)
+    
+    # All these functions will be imported from the 'app/functions' file
+    
+    
+    summary = summary("We are students") 
     primary = 'Primary viewpoint here' 
     secondary = 'Secondary viewpoint here'
     resources = 'Resources here'
