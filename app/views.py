0f0583921ebf0d39744ab3b4f2448decf5b49590
Robel Kebede <robel.kebede@bison.howard.edu>
2023-08-08 18:37:30 +0300

    Modified views.py based on the new features

--- app/views.py
+++ app/views.py
@@ -21,10 +21,15 @@ def analysis(request, instance_id):
     instance = TextInput.objects.get(id=instance_id)
     
     # Perform your analysis on instance.url here
-    # This is where our function would take the link (instance.url) and render a text which is the analyzed_output
-    analyzed_output = instance.url
+    # This is where our function would take the link (instance.url) and render our desired outputs
+    summary = instance.url
+    primary = 'Primary viewpoint here' 
+    secondary = 'Secondary viewpoint here'
+    resources = 'Resources here'
+    
+    
     if request.method == 'POST':
         instance.text = request.POST.get('edited_url')
         instance.save()
         return redirect('analysis', instance_id=instance_id)
-    return render(request, 'analysis.html', {'instance': instance, 'analyzed_output': analyzed_output})
+    return render(request, 'analysis.html', {'instance': instance, 'summary': summary, 'primary': primary, 'secondary': secondary, 'resources': resources})
