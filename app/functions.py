27f1e7a35aeab06bd3ac419f78f7247e0dfbe91c
Robel Kebede <robel.kebede@bison.howard.edu>
2023-08-09 22:32:44 +0300

    Add implementation of summary function

--- app/functions.py
+++ app/functions.py
@@ -4,18 +4,24 @@ from .WebScrape import WebScrape
 
 openai.api_key = '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'
 
+
+
+def generate_prompt(text):
+    return f"""You will summarize an article given the text from it, and remove any bias from the summary in the process. 
+    You will then give a rating based on how biased the original article was from one to ten, with ten being the most biased. At the end give sources for alternative news articles about the same event. {text.capitalize()}"""
+    
 # Function to make the summary
-def summary(text):
-    prompt =  """You will summarize an article given the text from it, and remove any bias from the summary in the process. 
-    You will then give a rating based on how biased the original article was from one to ten, with ten being the most biased.""".format(
-        text.capitalize()
-    )
-    response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=prompt,
-            temperature=0.6,
-        ).choices[0].text
-    return response
+# def summary(text):
+#     prompt =  """You will summarize an article given the text from it, and remove any bias from the summary in the process. 
+#     You will then give a rating based on how biased the original article was from one to ten, with ten being the most biased.""".format(
+#         text.capitalize()
+#     )
+#     response = openai.Completion.create(
+#             model="text-davinci-003",
+#             prompt=prompt,
+#             temperature=0.6,
+#         ).choices[0].text
+#     return response
 
 def primary_function(text):
     # Return a text of primary viewpoint
