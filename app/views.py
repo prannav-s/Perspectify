4b901b55543a16a26cd8c80f9c975a73a938fac9
Robel Kebede <robel.kebede@bison.howard.edu>
2023-08-11 12:34:26 +0300

    Added documentation details

--- app/views.py
+++ app/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from .forms import TextInputForm
 from .models import TextInput
-from .functions import WebScrape, generate_prompt
+from .functions import *
 import openai
 import re
 import os
 
 # Load environment variables from .env file
-
 env_file_path = "app/.env"
 
 # Read the API key from the .env file
@@ -19,20 +18,6 @@ with open(env_file_path, "r") as env_file:
             openai.api_key = line.strip().split("=")[1]
             break
 
-# openai.api_key_path = '/Users/robelmelaku/Desktop/Perspectify/app/.env'
-
-def extract_links_from_text(text):
-    # Using regular expression to find URLs in the text
-    urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
-    return urls
-
-def remove_alternative_sources_and_after(text):
-    # Remove all text after the sentence starting with "Alternative news sources: "
-    modified_text = text.split("Alternative news sources: ")[0]
-    return modified_text
-
-def index(request):
-    return render(request, 'index.html')
 
 def input_form(request):
     form = TextInputForm()
@@ -44,47 +29,15 @@ def input_form(request):
     return render(request, 'input_form.html', {'form': form})
 
 
-
-
 def analysis(request, instance_id):
     instance = TextInput.objects.get(id=instance_id)
     
-    # Perform our features on instance.url here
-    
 
-    # First we need to call the function that will scrape the link
-    # text = scrape_function(instance.url)
+    # Call the function that will scrape the link
     myscraper = WebScrape()
     web_page = myscraper.fetch_url(instance.url)
     text = myscraper.parse_html_content(web_page)
-    
-    
-    # Second we need to call the summary function to summarize the text
-    # summary = summary_function(text)
-    
-    # Third we need to call the primary and secondary viewpoints functions
-    # primary = primary_function(text)
-    # secondary = secondary_function(text)
-    
-    # Lastly, the resources function which will render the additional sources for the new viewpoints
-    # resources = resources(viewpoints)
-    
-    # All these functions will be imported from the 'app/functions' file
-    
-    
-    # The following lines will be later calling each function for each feature.
-    # These are just demos to show on the website
-    summary = 'Initial summary'
-    primary = text
-    secondary = text
-    resources = text.split('.')[:5] # Later will be links to actual sites.
-     
-    
-    # if request.method == 'POST':
-    #     instance.text = request.POST.get('edited_url')
-    #     instance.save()
-    #     return redirect('analysis', instance_id=instance_id)
-    
+
 
     response = openai.Completion.create(
         model="text-davinci-003",
@@ -93,20 +46,18 @@ def analysis(request, instance_id):
         max_tokens=500,  # Adjust this value as needed
 
     )
-    summary = "".join([i.text for i in response.choices ]) if response.choices else "No summary generated." 
-
-
-    summary_links = extract_links_from_text(summary)
     
-    resources = summary_links[2:]
+    # Call function to generate summary from the scraped text
+    summary = "".join([i.text for i in response.choices ]) if response.choices else "No summary generated." 
     summary = remove_alternative_sources_and_after(summary)
 
-    # primaryscraper = WebScrape()
-    # primarypage = primaryscraper.fetch_url(summary_links[2])
-    # primary = primaryscraper.parse_html_content(primarypage)
-    # secondaryscraper = WebScrape()
-    # secondarypage = secondaryscraper.fetch_url(summary_links[3])
-    # secondary = secondaryscraper.parse_html_content(secondarypage)
+    # Generate additional resources for requested source
+    summary_links = extract_links_from_text(summary)
+    resources = summary_links[2:]
+    
+    # Generate primary and secondary viewpoints for requested analysis
     primary = summary_links[0]
     secondary = summary_links[1]
+    
+    # Render each element using the written templates
     return render(request, 'analysis.html', {'instance': instance, 'summary': summary, 'primary': primary, 'secondary': secondary, 'resources': resources})
