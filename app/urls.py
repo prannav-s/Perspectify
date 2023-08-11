5e7df9c7a7e2a6540c34923dd8b1643a489d28e8
Robel Kebede <robel.kebede@bison.howard.edu>
2023-08-11 12:34:10 +0300

    Fixed URL tree

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('index/', views.index, name="index"),
     path('', views.input_form, name='input_form'),
     path('analysis/<int:instance_id>/', views.analysis, name='analysis'),
 ]
