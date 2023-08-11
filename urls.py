253e3ae137b7a778e453fbfa5d08b307bc7bc620
prannav-s <129468361+prannav-s@users.noreply.github.com>
2023-08-10 21:35:07 -0400

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('index/', views.index, name="index"),
-    path('', views.input_form, name='input_form'),
-    path('analysis/<int:instance_id>/', views.analysis, name='analysis'),
-]
